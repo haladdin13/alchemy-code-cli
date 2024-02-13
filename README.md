b5e59201d161e8f92669ceb5e3bd3f7fe5095271
Hussein Aladdin <haladdin13@outlook.com>
2024-02-13 10:35:06 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # alchemy-code-cli
 # This app will allow users to view, edit, create, and remove potion recipes.
+# Project Name Here
+Fill out your project details here in your README.md
---
+++ server/migrations/versions/2cc8750bba3b_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 2cc8750bba3b
+Revises: 
+Create Date: 2024-02-13 10:35:05.852435
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2cc8750bba3b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
